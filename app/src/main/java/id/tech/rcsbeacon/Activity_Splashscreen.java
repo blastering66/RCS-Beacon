b75cc78a940eaedaca42aff9e0dda4e8cd089679
ridho <ridho_maulana@outlook.com>
2016-02-16 13:49:52 +0700

    1st commit

---
+++ app/src/main/java/id/tech/rcsbeacon/Activity_Splashscreen.java
@@ -0,0 +1,13 @@
+package id.tech.rcsbeacon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
